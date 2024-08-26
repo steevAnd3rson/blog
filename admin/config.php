1727b4772e7c6f7d58344cb429b600e5ed87dea1
Steev And3rson <steev.and3rson@gmail.com>
2024-08-27 00:57:07 +0200

    configuracion

---
+++ admin/config.php
@@ -0,0 +1,18 @@
+<?php 
+define('RUTA', 'http://localhost/g/blog/');
+
+$bd_config = array(
+    'basedatos' => 'blog',
+    'usuario' => 'root',
+    'pass' => ''
+);
+
+$blog_config = array(
+    'post_por_pagina' => '2',
+    'carpeta_imagenes' => 'imagenes/'
+);
+
+$blog_admin = array(
+    'usuario' => 'admin',
+    'password' => ''
+);
---
+++ views/index.view.php
@@ -0,0 +1,8 @@
+<?php require 'header.php' ?>
+    
+
+<h1>Blog</h1>
+
+<?php require './paginacion.php' ?>
+    
+<?php require 'footer.php' ?>
